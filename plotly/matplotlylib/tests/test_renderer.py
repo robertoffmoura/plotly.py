37aedc180141869fecec9a4dbd571f976a13586a
Roberto Moura <roberto@ffmoura.com>
2025-06-15 18:21:35 +0100

    Add support for overriding x tick with non arithmetic progression values

--- plotly/matplotlylib/tests/test_renderer.py
+++ plotly/matplotlylib/tests/test_renderer.py
@@ -244,3 +244,27 @@ def test_line_color_is_valid_plotly_color():
     plotly_fig = tls.mpl_to_plotly(fig)
 
     assert plotly_fig.data[0].line.color == "rgba(255, 0, 0, 1)"
+
+
+def test_non_arithmetic_progression_xtickvals():
+    xticks = [0.01, 0.53, 0.75]
+    plt.figure()
+    plt.plot([0, 1], [0, 1])
+    plt.xticks(xticks)
+
+    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+
+    assert plotly_fig.layout.xaxis.tickvals == tuple(xticks)
+
+
+def test_non_arithmetic_progression_xticktext():
+    xtickvals = [0.01, 0.53, 0.75]
+    xticktext = ["Baseline", "param = 1", "param = 2"]
+    plt.figure()
+    plt.plot([0, 1], [0, 1])
+    plt.xticks(xtickvals, xticktext)
+
+    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+
+    assert plotly_fig.layout.xaxis.tickvals == tuple(xtickvals)
+    assert plotly_fig.layout.xaxis.ticktext == tuple(xticktext)
