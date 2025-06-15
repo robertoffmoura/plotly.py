4e9127320d55300935c4ed5814a430098f4bd86f
Roberto Moura <roberto@ffmoura.com>
2025-06-15 14:40:37 +0100

    Add lines+markers legend test

--- plotly/matplotlylib/tests/test_renderer.py
+++ plotly/matplotlylib/tests/test_renderer.py
@@ -575,3 +575,19 @@ def test_ticks_hidden_when_mpl_main_ticks_hidden():
 
     assert plotly_fig.layout.xaxis.ticks == ""
     assert plotly_fig.layout.yaxis.ticks == ""
+
+
+def test_lines_markers_legend_plot():
+    x = [0, 1]
+    y = [0, 1]
+    label = "label"
+    plt.figure()
+    plt.plot(x, y, "o-", label=label)
+    plt.legend()
+
+    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+
+    assert plotly_fig.data[0].mode == "lines+markers"
+    assert plotly_fig.data[0].x == tuple(x)
+    assert plotly_fig.data[0].y == tuple(y)
+    assert plotly_fig.data[0].name == "label"
