c96ca8703987797140e0085254497b0cf4e154f4
Roberto Moura <roberto@ffmoura.com>
2026-08-31 19:34:31 +0100

    Use `fig, ax = plt.subplots()` in tests

--- plotly/matplotlylib/tests/test_renderer.py
+++ plotly/matplotlylib/tests/test_renderer.py
@@ -248,11 +248,11 @@ def test_line_color_is_valid_plotly_color():
 
 def test_non_arithmetic_progression_xtickvals():
     xticks = [0.01, 0.53, 0.75]
-    plt.figure()
-    plt.plot([0, 1], [0, 1])
-    plt.xticks(xticks)
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 1])
+    ax.set_xticks(xticks)
 
-    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+    plotly_fig = tls.mpl_to_plotly(fig)
 
     assert plotly_fig.layout.xaxis.tickvals == tuple(xticks)
 
@@ -260,11 +260,11 @@ def test_non_arithmetic_progression_xtickvals():
 def test_non_arithmetic_progression_xticktext():
     xtickvals = [0.01, 0.53, 0.75]
     xticktext = ["Baseline", "param = 1", "param = 2"]
-    plt.figure()
-    plt.plot([0, 1], [0, 1])
-    plt.xticks(xtickvals, xticktext)
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 1])
+    ax.set_xticks(xtickvals, xticktext)
 
-    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+    plotly_fig = tls.mpl_to_plotly(fig)
 
     assert plotly_fig.layout.xaxis.tickvals == tuple(xtickvals)
     assert plotly_fig.layout.xaxis.ticktext == tuple(xticktext)
